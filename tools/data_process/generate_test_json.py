c8af720b48e43ecf120c6b2c4379b5cbc9cfa4bc
heqinglin336 <44044501+heqinglin336@users.noreply.github.com>
2020-04-10 13:11:54 +0800

    Update generate_test_json.py

--- tools/data_process/generate_test_json.py
+++ tools/data_process/generate_test_json.py
@@ -3,8 +3,9 @@ import os
 from glob import glob
 from tqdm import tqdm
 from PIL import Image
-from mmdet.core import underwater_classes
-label_ids = {name: i + 1 for i, name in enumerate(underwater_classes())}
+
+underwater_classes = ['holothurian', 'echinus', 'scallop', 'starfish']
+label_ids = {name: i + 1 for i, name in enumerate(underwater_classes)}
 
 
 def save(images, annotations):
@@ -17,7 +18,7 @@ def save(images, annotations):
     for k, v in label_ids.items():
         categories.append({"name": k, "id": v})
     ann['categories'] = categories
-    json.dump(ann, open('data/train/annotations/testA.json', 'w'))
+    json.dump(ann, open('data/coco/annotations/testB.json', 'w'))
 
 
 def test_dataset(im_dir):
@@ -27,7 +28,7 @@ def test_dataset(im_dir):
     images = []
     annotations = []
     for im_path in tqdm(im_list):
-        image_id += 1
+        
         im = Image.open(im_path)
         w, h = im.size
         image = {'file_name': os.path.basename(im_path), 'width': w, 'height': h, 'id': image_id}
@@ -40,10 +41,11 @@ def test_dataset(im_dir):
                    'bbox': bbox, 'category_id': 1, 'id': idx, 'ignore': 0}
             idx += 1
             annotations.append(ann)
+        image_id += 1
     save(images, annotations)
 
 
 if __name__ == '__main__':
-    test_dir = 'data/coco/test-A-image/'
+    test_dir = 'data/coco/test-B-image/'
     print("generate test json label file.")
     test_dataset(test_dir)
